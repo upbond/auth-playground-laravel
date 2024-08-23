16093a856a7789a1d286a99aa783d63a3b5ab5ee
Afifah <afifah@Soyus-MacBook-Pro.local>
2024-08-23 22:41:39 +0700

    plain

--- README.md
+++ README.md
@@ -17,20 +17,6 @@ php artisan serve --host localhost --port 8000
 
 Open in browser http://localhost:8000
 
-## User Info Related
-### Middleware 
-Middleware contains the function to get userinfo and verify JWT. Defined on [Userinfo.php](app/Http/Middleware/Userinfo.php)
-
-### APIs
-API that defined a /api/userinfo for GET and POST is on [api.php](/routes/api.php)
-- Route::get('/userinfo'...
-- Route::post('/userinfo'...
-
-### Call userinfo API service
-Contain on [Utils.php](app/Http/Helpers/Utils.php)
-- `getUserInfoUrl` function is to get userinfo URL from .well-known/openid-configuration
-- `getUserInfo` is to get userinfo from userinfo API that related with mysql database
-
 ## License
 
 This project is licensed under the MIT license by Auth0. See the [LICENSE](./LICENSE) file for more info.
@@ -57,20 +43,6 @@ php artisan serve --host localhost --port 8000
 ```
 
 ブラウザで開く http://localhost:8000
-
-## ユーザー情報関連
-### ミドルウェア 
-ミドルウェアには、ユーザー情報を取得し、JWTを検証する機能が含まれている。[Userinfo.php](app/Http/Middleware/Userinfo.php)で定義されています。
-
-### API
-GETとPOST用の/api/userinfoを定義したAPIは[api.php](/routes/api.php)にあります。
-- Route::get('/userinfo'...
-- Route::post('/userinfo'...
-
-### userinfo APIサービスを呼び出す
-[Utils.php](app/Http/Helpers/Utils.php)に記述。
-- getUserInfoUrl`関数は.well-known/openid-configurationからuserinfoのURLを取得する。
-- getUserInfo`関数は、mysqlデータベースに関連するuserinfo APIからユーザ情報を取得します。
 
 ## ライセンス
 
--- routes/web.php
+++ routes/web.php
@@ -21,38 +21,14 @@ Route::get('/', function () {
   $sessionData = json_decode(session()->get('auth0_session'));
   $idToken = $sessionData->idToken;
 
-  $user = Utils::getUserInfo($idToken);
-
-  $name = isset($user['name']) ? $user['name'] : 'User';
-  $email = isset($user['email']) ? $user['email'] : '';
+  // $user = Utils::getUserInfo($idToken);
+  // $name = isset($user['name']) ? $user['name'] : 'User';
+  // $email = isset($user['email']) ? $user['email'] : '';
 
   // - MOVE - USER GET FROM USERINFO API ON ABOVE FUNCTION
-  // $user = auth()->user(); 
-  // $name = $user->name ?? 'User';
-  // $email = $user->email ?? '';
+  $user = auth()->user(); 
+  $name = $user->name ?? 'User';
+  $email = $user->email ?? '';
 
   return view('logged_in', ['name' => $name, 'email' => $email, 'user' => $user, 'token' => $idToken]);
 });
-
-Route::get('/colors', function () {
-  $endpoint = Auth0::management()->users();
-
-  $colors = ['red', 'blue', 'green', 'black', 'white', 'yellow', 'purple', 'orange', 'pink', 'brown'];
-
-  $endpoint->update(
-    id: auth()->id(),
-    body: [
-      'user_metadata' => [
-        'color' => $colors[random_int(0, count($colors) - 1)]
-      ]
-    ]
-  );
-
-  $metadata = $endpoint->get(auth()->id()); // Retrieve the user's metadata.
-  $metadata = Auth0::json($metadata); // Convert the JSON to a PHP array.
-
-  $color = $metadata['user_metadata']['color'] ?? 'unknown';
-  $name = auth()->user()->name;
-
-  return response("Hello {$name}! Your favorite color is {$color}.");
-})->middleware('auth');
